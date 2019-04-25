5e0330983b4595b670619542360a0b7b75c2ab5e
Çıraq Rəsulov <chiraq.rasulov@bmsgl.local>
2019-04-25 05:24:51 -0700

    First Commit

---
+++ BusinessOneBarcodeGenerator/BarcodeGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessOneBarcodeGenerator
+{
+    public static class BarcodeGenerator
+    {
+        public static long GenerateEAN13(long code)
+        {
+            long barcode = 0;
+            try
+            {
+                if (code.ToString().Length == 12)
+                {
+                    var str = code.ToString();
+
+                    //convert string to int array
+                    var numbers = str.Select(c => c - '0').ToArray();
+                    int result1 = 0;
+                    int result2 = 0;
+
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            result1 += numbers[i];
+                        }
+                        else
+                        {
+                            result2 += numbers[i];
+                        }
+                    }
+
+                    int sum = (result1 * 3) + result2;
+                    //int sum2 = sum + result2;
+
+                    int RoundSum = (10 - sum % 10) + sum;
+                    int totalsum = RoundSum - sum;
+
+                    barcode = Convert.ToInt64(code + totalsum.ToString());
+                    return barcode;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return barcode;
+        }
+    }
+}
